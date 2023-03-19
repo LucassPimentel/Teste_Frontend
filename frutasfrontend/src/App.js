ca9897e86b4abdb690ce4b4919b59c1152e62dd2
Lucas Pimentel Da Silva <lupdsilva@simpress.com.br>
2023-03-19 17:04:16 -0300

    Detalhaes para estilização

--- frutasfrontend/src/App.js
+++ frutasfrontend/src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
-import Table from './Components/Table';
+import React from "react";
+import Table from "./Components/Table";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className='container'>
-      <Table />
+    <div
+      class="container d-flex justify-content-center align-items-center"
+      style={{ height: 100 + "vh" }}
+    >
+      <div
+        class="row text-center d-flex align-items-center"
+        style={{ width: 70 + "vw", height: 50 + "vh" }}
+      >
+        <Table />
+      </div>
     </div>
   );
 }
-
 export default App;
