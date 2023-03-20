a014458ea0e66bc41de164cfc0638dde4a5ea6cb
Lucas Pimentel Da Silva <lupdsilva@simpress.com.br>
2023-03-20 16:55:25 -0300

    Estilização modals

---
+++ frutasfrontend/src/style.jsx
@@ -0,0 +1,9 @@
+export const styles = {
+  CustomConfigModal: {
+    backgroundColor: "#2c3034",
+    width: "100%",
+    color: "white",
+    justifyContent: "center",
+  },
+  CustomConfigModalBody: { backgroundColor: "#2c3034", color: "white" },
+};
